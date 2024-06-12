3606353abd38544cd5d39e44bb3f7584456ea6bd
kakano22 <kakano@princeton.edu>
2024-06-12 14:45:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Create a base map object 
 //https://leafletjs.com/reference.html#map
-let map = L.map('map').setView([11.511488663931049, 18.89112567449333], 4.5); 
+let map = L.map('map').setView([11.511488663931049, 18.89112567449333], 2.5); 
 
 // Add a base layer to the map 
 // https://leafletjs.com/reference.html#tilelayer
