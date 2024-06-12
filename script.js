5b0d01475cf1eef2bca16980d1b86f07825f84cc
Gillian Macdonald <151566150+gilliansmac92@users.noreply.github.com>
2024-06-12 14:01:50 -0400

    Create script.js
    map

---
+++ script.js
@@ -0,0 +1,22 @@
+// Create a base map object 
+//https://leafletjs.com/reference.html#map
+let map = L.map('map').setView([39.952, -75.1932], 18); 
+
+// Add a base layer to the map 
+// https://leafletjs.com/reference.html#tilelayer
+L.tileLayer(
+  "https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+).addTo(map);
+
+
+
+
+//L.tileLayer.wms('https://maps.georeferencer.com/georeferences/71e4cdad-3140-5b47-8a08-514c206a844d/2022-12-23T23:48:40.740996Z/wmts?key=uL98Wi1mpNUAHIlUUqE6', {}).addTo(map);
+// "https://davidrumsey.oldmapsonline.org/maps/2dc03a45-da6e-509c-8bc4-c5407eb91517/view#802009941505"
+//"https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+// https://maps.georeferencer.com/georeferences/700526190853/2017-12-30T11:48:27.589686Z/map/{z}/{x}/{y}.png?key=D7AwmpRP1H6pUic6DIK3
+
+
+//L.esri.tiledMapLayer({
+//    url: "http://services.arcgisonline.com/ArcGIS/rest/services/USA_Topo_Maps/MapServer"
+//  }).addTo(map);
