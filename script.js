d4dabb5043e50d24218351550ea59d43be8052dc
Gillian Macdonald <151566150+gilliansmac92@users.noreply.github.com>
2024-06-13 10:44:13 -0400

    Update script.js
    Adding search on map

--- script.js
+++ script.js
@@ -66,4 +66,9 @@ noUiSlider.create(slider, {
       stepped: true,
       density: 4
     } 
+      
+  var searchLayer = L.layerGroup().addTo(map);
+//... adding data in searchLayer ...
+map.addControl( new L.Control.Search({layer: searchLayer}) );
+//searchLayer is a L.LayerGroup contains searched markers
 });
